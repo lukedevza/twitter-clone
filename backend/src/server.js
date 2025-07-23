e2de4abb632e7f0763feb883d966f9342474f625
Luke Storti <lukestorti@gmail.com>
2025-07-23 14:58:58 +0200

    post routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 const app = express();
 
@@ -15,6 +16,13 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("Hello from server"));
 
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+// error handling middleware
+app.use((err, req, res) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
 
 const startServer = async () => {
   try {
