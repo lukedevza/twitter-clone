7b19f34150d001f4a5cc31abeeaa4e913484ea87
Luke Storti <lukestorti@gmail.com>
2025-07-24 13:17:13 +0200

    search tab added

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -21,8 +21,8 @@ const TabsLayout = () => {
           borderTopColor: "#E1E8ED",
           height: 50 + insets.bottom,
           paddingTop: 8,
-          borderTopLeftRadius: 25,
-          borderTopRightRadius: 25,
+          borderTopLeftRadius: 34,
+          borderTopRightRadius: 34,
         },
 
         headerShown: false,
